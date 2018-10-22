9eebb2f5d3a74413056125bd34842363c30794f7
khadak.k <khadak.k@flock.com>
2018-10-22 16:30:44 +0530

    first commit

---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Sidbar extends Component{
+    render () {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Sidbar;
