94f85cfd314b1bbebfeade23b31b14dff9e5d17b
khadak.k <khadak.k@flock.com>
2018-10-23 22:40:02 +0530

    home relative path add

---
+++ precache-manifest.48274c73cd0834dc1e6234a8322da22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52305990958b49d2ebe76494bb277912",
+    "url": "./static/media/logo.52305990.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9533e6fee424f2537c26",
+    "url": "./static/js/main.9533e6fe.chunk.js"
+  },
+  {
+    "revision": "e680433d83434ed999ed",
+    "url": "./static/js/1.e680433d.chunk.js"
+  },
+  {
+    "revision": "9533e6fee424f2537c26",
+    "url": "./static/css/main.6204a2ba.chunk.css"
+  },
+  {
+    "revision": "89a9d87630855eefd955f53321fdc583",
+    "url": "./index.html"
+  }
+];
