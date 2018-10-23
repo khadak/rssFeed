a65deda00bcc316bbc33de9417b70bbf9092f34e
khadak.k <khadak.k@flock.com>
2018-10-23 22:26:29 +0530

    Initial build subtree commit

---
+++ build/precache-manifest.cbfa01894196971cd6c58fc5b4834224.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52305990958b49d2ebe76494bb277912",
+    "url": "/static/media/logo.52305990.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9533e6fee424f2537c26",
+    "url": "/static/js/main.9533e6fe.chunk.js"
+  },
+  {
+    "revision": "2efd2d49d41b1c19ab03",
+    "url": "/static/js/1.2efd2d49.chunk.js"
+  },
+  {
+    "revision": "9533e6fee424f2537c26",
+    "url": "/static/css/main.6204a2ba.chunk.css"
+  },
+  {
+    "revision": "78768a26a6fcaf6c52fb43492ab25116",
+    "url": "/index.html"
+  }
+];
